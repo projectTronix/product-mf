058399ddc87b77fe1a6e549598d2683cf9626d7f
Mayank Rawat <mrawat@fastenal.com>
2024-03-09 22:29:53 +0530

    fix add to cart

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -6,11 +6,17 @@ import "./AddToCart.scss";
 
 const AddToCart = ({ productId, quantity, cart, setCart }) => {
   const handleAddToCart = async () => {
+    if (localStorage.getItem("token") == undefined) {
+      toast.success("Kindly Login.", {
+        autoClose: 1000,
+      });
+      return;
+    }
     try {
       const response = await api.post("http://localhost:8091/cart/update", {
         productId,
         quantity: quantity,
-        quantityAdd: quantity
+        quantityAdd: quantity,
       });
       if (response.status == 200) {
         setCart([...cart, productId]);
@@ -18,13 +24,14 @@ const AddToCart = ({ productId, quantity, cart, setCart }) => {
           autoClose: 1000,
         });
       }
-      
     } catch (error) {
       console.log(error);
     }
   };
   return (
-    <div className="d-grid gap-2 product-btn-container position-relative"> {console.log(cart)}
+    <div className="d-grid gap-2 product-btn-container position-relative">
+      {" "}
+      {console.log(cart)}
       <Button size="lg" className="product-btn" onClick={handleAddToCart}>
         <span className="position-absolute start-0">+</span>Add to Cart
       </Button>
